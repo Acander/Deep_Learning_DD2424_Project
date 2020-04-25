e25af0e2592ed02ec07a0e66da170376408203ed
JoeyOhman <joeyoh@kth.se>
2020-04-25 17:49:58 +0200

    asd

--- TestTensorFlow/transferLearning.py
+++ TestTensorFlow/transferLearning.py
@@ -1,4 +1,11 @@
 # Tutorial: https://www.tensorflow.org/tutorials/images/transfer_learning
 
+import os
+
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from tensorflow import data
+tfds.disable_progress_bar()
 
 
