57c496ae43a3b1d5355362a3382564cfe5b5168f
JoeyOhman <joeyoh@kth.se>
2020-04-25 19:38:52 +0200

    Transfer Learning and Fine-Tuning

--- TestTensorFlow/transferLearning.py
+++ TestTensorFlow/transferLearning.py
@@ -46,7 +46,7 @@ def run():
         plt.figure()
         plt.imshow(image)
         plt.title(get_label_name(label))
-        plt.show()
+        # plt.show()
 
     train = raw_train.map(format_example)
     validation = raw_validation.map(format_example)
@@ -71,11 +71,100 @@ def run():
     print(feature_batch.shape)
 
     base_model.trainable = False
-    base_model.summary()
+    # base_model.summary()
 
+    # ADD POOLING FOR LOWER DIMENSIONS ON HEAD NETWORK INPUT, AND ADD DENSE OUTPUT
     global_average_layer = tf.keras.layers.GlobalAveragePooling2D()
     feature_batch_average = global_average_layer(feature_batch)
     print(feature_batch_average.shape)
+
+    prediction_layer = tf.keras.layers.Dense(1)
+    prediction_batch = prediction_layer(feature_batch_average)
+    print(prediction_batch.shape)
+
+    model = tf.keras.Sequential([
+        base_model,
+        global_average_layer,
+        prediction_layer
+    ])
+
+    base_learning_rate = 0.0001
+    model.compile(optimizer=tf.keras.optimizers.RMSprop(lr=base_learning_rate),
+                  loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
+                  metrics=['accuracy'])
+
+    model.summary()
+
+    initial_epochs = 10
+    validation_steps = 20
+
+    loss0, accuracy0 = model.evaluate(validation_batches, steps=validation_steps)
+
+    print("initial loss: {:.2f}".format(loss0))
+    print("initial accuracy: {:.2f}".format(accuracy0))
+
+    history = model.fit(train_batches,
+                        epochs=initial_epochs,
+                        validation_data=validation_batches)
+
+    acc = history.history['accuracy']
+    val_acc = history.history['val_accuracy']
+
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+
+    # FINE TUNE
+    base_model.trainable = True
+
+    # Let's take a look to see how many layers are in the base model
+    print("Number of layers in the base model: ", len(base_model.layers))
+
+    # Fine-tune from this layer onwards
+    fine_tune_at = 100
+
+    # Freeze all the layers before the `fine_tune_at` layer
+    for layer in base_model.layers[:fine_tune_at]:
+        layer.trainable = False
+
+    model.compile(loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
+                  optimizer=tf.keras.optimizers.RMSprop(lr=base_learning_rate / 10),
+                  metrics=['accuracy'])
+
+    fine_tune_epochs = 10
+    total_epochs = initial_epochs + fine_tune_epochs
+
+    history_fine = model.fit(train_batches,
+                             epochs=total_epochs,
+                             initial_epoch=history.epoch[-1],
+                             validation_data=validation_batches)
+
+    acc += history_fine.history['accuracy']
+    val_acc += history_fine.history['val_accuracy']
+
+    loss += history_fine.history['loss']
+    val_loss += history_fine.history['val_loss']
+
+    # PLOT RESULTS
+    plt.figure(figsize=(8, 8))
+    plt.subplot(2, 1, 1)
+    plt.plot(acc, label='Training Accuracy')
+    plt.plot(val_acc, label='Validation Accuracy')
+    plt.ylim([0.8, 1])
+    plt.plot([initial_epochs - 1, initial_epochs - 1],
+             plt.ylim(), label='Start Fine Tuning')
+    plt.legend(loc='lower right')
+    plt.title('Training and Validation Accuracy')
+
+    plt.subplot(2, 1, 2)
+    plt.plot(loss, label='Training Loss')
+    plt.plot(val_loss, label='Validation Loss')
+    plt.ylim([0, 1.0])
+    plt.plot([initial_epochs - 1, initial_epochs - 1],
+             plt.ylim(), label='Start Fine Tuning')
+    plt.legend(loc='upper right')
+    plt.title('Training and Validation Loss')
+    plt.xlabel('epoch')
+    plt.show()
 
 
 if __name__ == '__main__':
